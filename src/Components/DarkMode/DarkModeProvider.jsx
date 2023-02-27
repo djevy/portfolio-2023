4d5e2a3523acabfbbf0105f1dbde3d588b0df737
Darren <60827567+djevy@users.noreply.github.com>
2023-02-27 21:25:54 +0000

    Added local storage to dark theme change

--- src/Components/DarkMode/DarkModeProvider.jsx
+++ src/Components/DarkMode/DarkModeProvider.jsx
@@ -3,17 +3,27 @@ import React, { createContext, useState } from "react";
 const DarkModeContext = createContext();
 
 const DarkModeProvider = (props) => {
-  const [darkMode, SetDarkMode] = useState(false);
+  const [theme, SetTheme] = useState(localStorage.getItem("theme") || 'light-mode');
+  const [darkMode, SetDarkMode] = useState(theme === "light-mode" ? true : false);
   const toggleDarkMode = () => {
-    SetDarkMode(!darkMode);
+    if (theme === 'light-mode') {
+      SetDarkMode(false);
+      SetTheme('dark-mode');
+      localStorage.setItem('theme', 'dark-mode');
+    } else {
+      SetDarkMode(true);
+      SetTheme('light-mode');
+      localStorage.setItem('theme', 'light-mode');
+    }
+    
   };
   return (
     <div className={darkMode ? "light-mode" : ""}>
-      <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
+      <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
         {props.children}
       </DarkModeContext.Provider>
     </div>
   );
 };
 
-export {DarkModeContext, DarkModeProvider};
+export { DarkModeContext, DarkModeProvider };
